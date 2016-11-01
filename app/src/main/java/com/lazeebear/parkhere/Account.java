10a5c7ad14dd99e82d105cc358c82c59ce9a8fed
Ryosuke Mitsubayashi <mitsubay@usc.edu>
2016-11-01 13:10:15 -0700

    saving user information locally

--- app/src/main/java/com/lazeebear/parkhere/Account.java
+++ app/src/main/java/com/lazeebear/parkhere/Account.java
@@ -22,16 +22,6 @@ import com.google.android.gms.common.api.GoogleApiClient;
 
 public class Account extends AppCompatActivity {
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    //private GoogleApiClient client;
-
-    private String user_phonenumber;
-    private int user_stars;
-    private String user_name;
-    private String user_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,21 +30,6 @@ public class Account extends AppCompatActivity {
         hideComponentsOnStartUp();
         fillInformationOnStartUp();
         addActionListeners();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-
-        //defaults
-        user_phonenumber = "9494994949";
-        user_stars = 3;
-        user_email = "jimmy@aol.com";
-
-        Intent intent = getIntent();
-        //if intent != null
-        user_name = intent.getStringExtra("");
-        user_stars = intent.getIntExtra("", 0); //second int is the default int, if intent didn't send one with it.
-        user_email = intent.getStringExtra("");
-        user_phonenumber = intent.getStringExtra("");
     }
 
     private void hideComponentsOnStartUp() {
@@ -102,13 +77,13 @@ public class Account extends AppCompatActivity {
         TextView accountName = (TextView) findViewById(R.id.accountName_account);
         accountName.setText(getDisplayName());
         RatingBar ratingOfUser = (RatingBar) findViewById(R.id.ratingBar);
-        ratingOfUser.setNumStars(user_stars);
+        ratingOfUser.setNumStars(User.rating);
 
         TextView phoneNumber = (TextView) findViewById(R.id.phoneNumber);
-        phoneNumber.setText(user_phonenumber);
+        phoneNumber.setText(User.phoneNumber);
 
         TextView email = (TextView) findViewById(R.id.email);
-        email.setText(user_email);
+        email.setText(User.email);
     }
 
 
@@ -232,14 +207,12 @@ public class Account extends AppCompatActivity {
     }
 
     private String getDisplayName() {
-        //split user_name accordingly:
-        String[] output = user_name.split(" "); //split along the space
         if (isViewingOwnAccount())
             //return "First and Last Name";
-            return user_name;
+            return User.firstName + " " + User.lastName;
         else
             //return "First Name Only";
-            return output[0];
+            return User.firstName;
     }
 
     /**
